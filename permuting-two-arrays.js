830e63efcf48b37f19bc025f195ba3c3a8eb7b52
Emerson <edwarhman@gmail.com>
2021-09-29 19:39:00 -0400

    upload subarray challenge and update permuting array challenge

--- permuting-two-arrays.js
+++ permuting-two-arrays.js
@@ -1,37 +1,12 @@
-function countRepeats(arr) {
-  const max = arr.reduce((acc, el) => el > acc? el : acc);
-  return arr.reduce((acc, el) => {
-    acc[el]++;
-    return acc;
-  }, Array(max+1).fill(0,0));
-}
-
 function twoArrays(k, A, B) {
-  // Write your code here
-  A.sort().reverse();
-  B.sort();
-  console.log(A, B);
+  A.sort((a, b) => b - a);
+  B.sort((a, b) => a - b);
   const sum = A.map((el, index) => el + B[index]);
   return sum.every(el => el >= k)? "YES": "NO";
 }
-function twoArrays2(k, A, B) {
-  const min = A.reduce((acc, el) => el < acc ? el : acc);
-  const max = B.reduce((acc, el) => el > acc ? el : acc);
-  let repeatsInA = countRepeats(A);
-  let repeatsInB = countRepeats(B);
-  console.log(repeatsInA, repeatsInB);
 
-  repeatsInA.forEach((element, index) => {
-    console.log(repeatsInB[max  - index])
-    if (element === repeatsInB[max + 1 - index]) {
-      console.log("as");
-    }
-  });
-}
-let arr = [1,1,2,3,1];
 const A = "84 2 50 51 19 58 12 90 81 68 54 73 81 31 79 85 39 2".split(" ").map(el => Number(el));
 const B = "53 102 40 17 33 92 18 79 66 23 84 25 38 43 27 55 8 19".split(" ").map(el => Number(el));
 const k = 88;
 console.log(A, B);
-// console.log(twoArrays(10, [2,1,3], [7,8,9]));
 console.log(twoArrays(k, A, B));
--- subarray-division.js
+++ subarray-division.js
@@ -1,20 +1,4 @@
-function birthday(a, d, m) {
-    // Write your code here
-  console.log(a);
-  let s = [...a];
-  let spliceCount = 0;
-  return s.reduce((acc, el, index) => {
-    let sum = s.slice(index - spliceCount, index - spliceCount + m).reduce((acc, el) => acc + el, 0);
-    if (sum === d) {
-      s.splice(index, index + m);
-      acc++;
-      spliceCount++;
-    }
-    return acc;
-  }, 0);
-}
-
-function birthday2(s, d, m) {
+function birthday(s, d, m) {
   let nop = 0;
   for (let i = 0; i < s.length - (m-1); i++) {
     const sum = s.slice(i, i + m).reduce((acc,el) => acc + el, 0);
